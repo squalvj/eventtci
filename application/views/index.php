e729ee7da390e35900b9f24b67c34338e11596f8
squalvj <squalvj@gmail.com>
2017-06-17 22:33:10 +0700

    clear section 1 - 2 tinggal benerin bug navbar

--- application/views/index.php
+++ application/views/index.php
@@ -10,49 +10,123 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<title>Event Topcareer.id</title>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 		<link href="<?php echo base_url(); ?>assets/css/css.css" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Oxygen|Patua+One|Quicksand|Raleway|Righteous" rel="stylesheet">
+		<link href="<?php echo base_url(); ?>assets/bower_components/swiper/dist/css/swiper.min.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Oxygen|Patua+One|Quicksand|Raleway|Righteous|Montserrat|Rubik+Mono+One" rel="stylesheet">
 	</head>
 	<body>
-		<div id="section1" class="container-fluid no-padding">
-			<div class="parent-section1">
-				<div class="row">
-					<div class="col-md-2">
-						<img class="logotci" src="<?php echo base_url(); ?>assets/img/logo.png">
+		<nav class="navbar navbar-fixed-top navbar--color--ghost navbar--size--xl" data-startColor="navbar--color--ghost" data-startSize="navbar--size--xl" data-intoColor="navbar--color--secondary" data-intoSize="navbar--size--md">
+		  <div class="container">
+		    <div class="navbar-header">
+		      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#mobileNav" aria-expanded="false">
+		      	<span class="sr-only">Toggle navigation</span>
+		      	<span class="icon-bar"></span>
+		      	<span class="icon-bar"></span>
+		      	<span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">
+		      	<img alt="Brand" src="<?php echo base_url(); ?>assets/img/logo.png">
+		      </a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="mobileNav">
+		      <div class="navigasi navbar-right">
+				<div class="item-nav">
+					<button>Speaker</button>
+				</div>
+				<div class="item-nav">
+					<button>Exhibitor</button>
+				</div>
+				<div class="item-nav">
+					<button>Ticket Info</button>
+				</div>
+				<div class="item-nav">
+					<button>Venus</button>
+				</div>
+			</div>
+		    </div>
+		  </div>
+		</nav>
+		<div class="section-utama">
+			<div class="swiper-container">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide swiper-slide-1">
+						<div class="container">
+							<div class="wrapper-slider">
+								<h2>Memilih Perguruan Tinggi berdasarkan minat dan bakat</h2> 
+									<p>Bidang pekerjaan dengan masa depan cerah</p>
+
+									<div class="wrapper-detail-jumbotron">
+										<h4>Saturday, Nov 25Th 2017</h4>
+										<h4>10.00 am - 4.00 pm</h4>
+										<button class="btn-jumbotron red-btn">Free Tickets</button>
+										<button class="btn-jumbotron">Featured Speaker</button>
+									</div>
+							</div>
+						</div>
 					</div>
-					<div class="col-md-8 col-md-offset-2">
-						<div class="navigasi">
-							<div class="item-nav">
-								<button>What is</button>
+					<div class="swiper-slide swiper-slide-2">
+						<div class="container">
+								<div class="wrapper-slider">
+									<h2>Berkarya demi Indonesia atau negara lain</h2> 
+										<p>Ketika pajak rakyat disalahgunakan oleh penerima beasiswa</p>
+
+										<div class="wrapper-detail-jumbotron">
+											<h4>Saturday, Nov 25Th 2017</h4>
+											<h4>10.00 am - 4.00 pm</h4>
+											<button class="btn-jumbotron red-btn">Free Tickets</button>
+											<button class="btn-jumbotron">Featured Speaker</button>
+										</div>
+								</div>
 							</div>
-							<div class="item-nav">
-								<button>Speaker</button>
+					</div>
+					<div class="swiper-slide swiper-slide-3">
+						<div class="container">
+							<div class="wrapper-slider">
+								<h2>Jalur berkarier diluar negeri</h2> 
+									<p>Jasa perantara atau penerima langsung?</p>
+
+									<div class="wrapper-detail-jumbotron">
+										<h4>Saturday, Nov 25Th 2017</h4>
+										<h4>10.00 am - 4.00 pm</h4>
+										<button class="btn-jumbotron red-btn">Free Tickets</button>
+										<button class="btn-jumbotron">Featured Speaker</button>
+									</div>
 							</div>
-							<div class="item-nav">
-								<button>Exhibitor</button>
+						</div>
+					</div>
+					<div class="swiper-slide swiper-slide-4">
+						<div class="container">
+							<div class="wrapper-slider">
+								<h2>Daftar beasiswa tahun 2018</h2> 
+									<p>Ciri penerima beasiswa</p>
+
+									<div class="wrapper-detail-jumbotron">
+										<h4>Saturday, Nov 25Th 2017</h4>
+										<h4>10.00 am - 4.00 pm</h4>
+										<button class="btn-jumbotron red-btn">Free Tickets</button>
+										<button class="btn-jumbotron">Featured Speaker</button>
+									</div>
 							</div>
-							<div class="item-nav">
-								<button>Ticket Info</button>
-							</div>
-							<div class="item-nav">
-								<button>Venus</button>
+						</div>
+					</div>
+					<div class="swiper-slide swiper-slide-5">
+						<div class="container">
+							<div class="wrapper-slider">
+								<h2>Sharing session: Dino Patti Djalal</h2> 
+									<p>Menuju Indonesia Global</p>
+									<div class="wrapper-detail-jumbotron">
+										<h4>Saturday, Nov 25Th 2017</h4>
+										<h4>10.00 am - 4.00 pm</h4>
+										<button class="btn-jumbotron red-btn">Free Tickets</button>
+										<button class="btn-jumbotron">Featured Speaker</button>
+									</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="jumbotron jumbo-1">
-				<div class="wrapper-jumbotron">
-					<h2>Memilih Perguruan Tinggi berdasarkan minat dan bakat</h2> 
-					<p>Bidang pekerjaan dengan masa depan cerah</p>
-
-					<div class="wrapper-detail-jumbotron">
-						<h4>Saturday, Nov 25Th 2017</h4>
-						<h4>10.00 am - 4.00 pm</h4>
-						
-						<button class="btn-jumbotron">Free Tickets</button>
-						<button class="btn-jumbotron">Featured Speaker</button>
-					</div>
-				</div>
+				<div class="swiper-button-prev swiper-button-white"></div>
+				<div class="swiper-button-next swiper-button-white"></div>
+				<div class="swiper-pagination swiper-pagination-white"></div>
+				<div class="swiper-scrollbar"></div>
 			</div>
 		</div>
 
@@ -60,15 +134,68 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div id="section2">
 				<h1 class="text-center">Connecting Stakeholders of Global Education</h1>
 				<h4 class="text-center">Dihadiri oleh puluhan booth dibidang pendidikan yang berasal dari dalam dan luar negeri, directors of relevant government agencies and key influential figures in the industry, the Global Career & Education Fair 2017 aims to accelerate the growth of the countr's human resources by gathering key stakeholders and inspiring participants to grow their country</h4>
+				<div class="wrapper-5 row">
+					<div class="col-xs-offset-1 col-xs-2 col-md-2 col-sm-2 col-sm-offset-1 col-md-offset-1">
+						<div class="wrapper-icon text-center">
+							<img class="ico" src="<?php echo base_url(); ?>assets/img/gov.png">
+							<h5>Government</h5>
+						</div>
+					</div>
+			    <div class="col-md-2 col-sm-2 col-xs-2">
+			    	<div class="wrapper-icon text-center">
+							<img class="ico" src="<?php echo base_url(); ?>assets/img/gov.png">
+							<h5>Scholarship</h5>
+						</div>
+			    </div>
+			    <div class="col-md-2 col-sm-2 col-xs-2">
+			    	<div class="wrapper-icon text-center">
+							<img class="ico" src="<?php echo base_url(); ?>assets/img/gov.png">
+							<h5>College</h5>
+						</div>
+			    </div>
+			    <div class="col-md-2 col-sm-2 col-xs-2">
+			    	<div class="wrapper-icon text-center">
+							<img class="ico" src="<?php echo base_url(); ?>assets/img/gov.png">
+							<h5>Education Services</h5>
+						</div>
+			    </div>
+			    <div class="col-md-2 col-sm-2 col-xs-2">
+			    	<div class="wrapper-icon text-center">
+							<img class="ico" src="<?php echo base_url(); ?>assets/img/gov.png">
+							<h5>Scholarship Grantee</h5>
+						</div>
+			    </div>
+				</div>
+			</div>
+		</div>
+
+		<div id="section3" class="container-fluid">
+			<div class="wrapper-fluid">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="flex-column">
+							
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="flex-column">
+
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="flex-column">
+
+						</div>
+					</div>	
+				</div>
 			</div>
 		</div>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="https://cdn.rawgit.com/pixelcog/parallax.js/master/parallax.min.js"></script>
 		<script src="<?php echo base_url();?>assets/js/j.js"></script>
-		<script type="text/javascript">
-			$('.jumbo-1').parallax({imageSrc: '<?php echo base_url();?>assets/img/p.png'});
-		</script>
+		<script src="<?php echo base_url();?>assets/js/countUp.js"></script>
+		<script src="<?php echo base_url();?>assets/bower_components/swiper/dist/js/swiper.min.js"></script>
+		<script src="<?php echo base_url();?>assets/js/parallax.min.js"></script>
 	</body>
 </html>
